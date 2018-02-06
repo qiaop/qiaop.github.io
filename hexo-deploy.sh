39f1d4a53ff3ddbcd674a52cf64a14b58fffb488
qiaop <qiaopeng0809@gmail.com>
2018-02-06 18:03:32 +0800

    publish post

--- hexo-deploy.sh
+++ hexo-deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 git add . &&
+wait
 echo "git add ." &&
-
+wait
 git commit -m "publish post" &&
-
+wait
 echo "git commit " &&
-
+wait
 git push origin hexo &&
-
+wait
 echo "git push origin hexo" &&
 
 echo "start deploy" &&
